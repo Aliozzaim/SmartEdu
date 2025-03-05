076ba3139073b3064ffe90f8cb87c075d79a8d94
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2025-03-05 18:42:40 +0100

    initial comit

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,2 +1,9 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies for Python
+echo "Installing Python dependencies..."
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Run Django collectstatic to gather all static files into the staticfiles directory
+python3 manage.py collectstatic --noinput
